21d898f7da64d3197906dacce54106db15f71908
Sanjay Gopal Shrestha <sanjay.gopal.shrestha@gmail.com>
2026-02-14 13:39:35 +0000

    Update navbar logo text to "Sanjshres."

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -5,7 +5,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="navbar p-0 m-auto container d-flex justify-content-center align-items-center">
       <div className="navbar-container d-flex justify-content-between align-items-center w-100">
-        <div className="navbar-logo">Sanjay.</div>
+        <div className="navbar-logo">Sanjshres.</div>
         <button className="navbar-button">
           <ChatTeardropDotsIcon />
         </button>
